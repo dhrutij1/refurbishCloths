0817df210ff4a8d8eeadfd6c75b897d0f37f0c20
abhishekpanchal <dhruti.joshi@sooryen.com>
2017-02-07 17:40:55 +0530

    Add Application.java class

---
+++ src/main/java/com/indiatech/config/Application.java
@@ -0,0 +1,15 @@
+package com.indiatech.config;
+
+/**
+ * Created by Dhruti on 07/02/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
